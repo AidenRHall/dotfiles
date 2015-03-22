1d0242b22adbd20a6ac9d6de391c3e8ff15bc278
aiden <aiden@spare>
2015-03-22 13:26:43 -0400

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,36 @@
-if [ ! -d ~/.dotfiles ]; then
-    echo "exiting: ~/.dotfiles does not exist"
+if [ ! -d $HOME/.dotfiles ]; then
+    echo "exiting: $HOME/.dotfiles does not exist"
     exit 1
 fi
 
 declare -a FILES=('.vimrc'
                   '.vim'
                   '.zshrc'
+                  '.oh-my-zsh'
                   '.aliases')
 
 for FILE in "${FILES[@]}"
 do
-    if [ -f ~/.dotfiles/$FILE ]; then
-        if [ -f ~/$FILE ]; then
-            rm ~/$FILE
+    if [ -f $HOME/.dotfiles/$FILE ]; then
+        if [ -f $HOME/$FILE ]; then
+            rm $HOME/$FILE
         fi
-        ln -s ~/.dotfiles/$FILE ~/$FILE
+        ln -s $HOME/.dotfiles/$FILE $HOME/$FILE
         echo "$FILE: symlinked"
     else
         echo "$FILE: does not exist"
     fi
 done
 
-mkdir ~/.vim/backups
-mkdir ~/.vim/undodir
+mkdir $HOME/.vim/backups
+mkdir $HOME/.vim/undodir
+
+mkdir $HOME/.vim/bundle
+git clone https://github.com/gmarik/Vundle.vim .vim/bundle/Vundle.vim
+vim +BundleInstall +qall
+
+git clone https://github.com/powerline/fonts.git
+bash fonts/install.sh
+rm -rf fonts
 
 echo 'done setting up'
